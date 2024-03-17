53847133e9d96dfa829f7ee6c1227fcacd9c9142
Raj Garg <mothership@raj.local>
2024-03-17 17:37:34 +0530

    super simple bigram language model

--- bigram.py
+++ bigram.py
@@ -64,7 +64,7 @@ def estimate_loss():
 # super simple bigram language model
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
@@ -101,7 +101,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
     
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 # creating a PyTorch optimizer
